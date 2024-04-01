7ced0af77fa873b0a76b4edbeed6fe2dd2e8822c
Muhammad Saad Afzal <152029259+codersyntanx@users.noreply.github.com>
2024-04-01 19:04:40 +0500

    update

--- src/App.js
+++ src/App.js
@@ -25,49 +25,7 @@ const handledeletetodo = (id)=>{
   return (
     <div className="App">
     
-      <h1>TODO LIST</h1>
-<input type='text' value={addtodos} onChange={(e)=>{setAddtodos(e.target.value)}}
-placeholder='Add your work...'></input>
-<button onClick={handletodo}>Add Todo</button>
-
-<ul>
-  {
-    todos.map((item)=>{
-      return(
-        <>
-        <li className='cover' key={item.id}      style={{textDecoration:item.done? "line-through":"none"}}
-     
->
-  {item.text}
-  <button onClick={()=>{handledeletetodo(item.id)}}>  Delete the item</button>
-  <button   onClick={()=>handletoggletodo(item.id)} >
-    {
-      item.done?(<>  Mark as Undone</>):(<>Mark as done</>)
-    }
-  </button>
-        </li>
-      
-        </>
-      )
-    })
-  }
-{/* {
-  todos.map((todo)=>{
-    return(
-      <>
-      <li 
-      key={todo.id}
-      style={{textDecoration:todo.done? "line-through":"none"}}
-      onClick={handletoggletodo(todo.id)}
-      >
-
-      </li>
-      
-      </>
-    )
-  })
-} */}
-</ul>
+  
 
 <Routes>
   <Route path='/' element={<Propsgetter />}/>
--- src/Propsgetter.jsx
+++ src/Propsgetter.jsx
@@ -1,14 +1,76 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
-
+import { useDispatch, useSelector } from 'react-redux';
+import { addTodo, removetodo, toggletodo } from './redux/reducers';
+import { Modal } from "antd";
 function Propsgetter (props){
-   
+    const [addtodos,setAddtodos]=useState("")
+    const [ismodalopen,setIsmodalopen]=useState(false)
+ const dispatch = useDispatch()
+ const todos = useSelector((state)=>state.todo)
+const handletodo =()=>{
+  if(addtodos.trim() !== ""){
+    dispatch(addTodo(addtodos.trim()))
+    setAddtodos("")
+  }
+}
+const handletoggletodo = (id)=>{
+  dispatch(toggletodo(id))
+}
+const handledeletetodo = (id)=>{
+  dispatch(removetodo(id))
+}
     return(
         <>
-                <h1>This is Home page</h1>
-<h1>{props.name}</h1>
-        This is the props page<br></br>
-        <Link to="/about">Go to About Page</Link>
+                    
+      <h1>TODO LIST</h1>
+<input type='text' className="inputtake" value={addtodos} onChange={(e)=>{setAddtodos(e.target.value)}}
+placeholder='Add your work...'></input>
+<button className="addtodobtn" onClick={handletodo}>Add Todo</button>
+
+    <ul>
+  {
+    todos.map((item)=>{
+      return(
+        <>
+        <div className="coverofli">
+        <li className='cover' key={item.id}      style={{textDecoration:item.done? "line-through":"none"}}
+     
+>
+    <div style={{display:"flex",gap:"10px"}}>
+         <div className="coveroficon" onClick={()=>{handledeletetodo(item.id)}}>
+         <i class="fa-regular fa-circle-xmark"></i>
+    </div>
+
+    <div className="item">
+         {item.text}
+    </div>
+    </div>
+    <div onClick={()=>handletoggletodo(item.id)} >
+        {
+            item.done?(<><i class="fa-regular fa-circle-check"style={{color:"green"}}></i></>):(<><i class="fa-regular fa-circle" ></i></>)
+        }
+    </div>
+   
+ 
+  {/* <button >  Delete the item</button> */}
+  {/* <button   onClick={()=>handletoggletodo(item.id)} >
+    {
+      item.done?(<>  Mark as Undone</>):(<>Mark as done</>)
+    }
+  </button> */}
+        </li>
+        </div>
+        </>
+      )
+    })
+  }
+
+</ul>
+<Modal>
+
+</Modal>
+
         </>
     )
 }
